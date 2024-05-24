23444a835be8b7d4367e694c19833fe8366eff3e
hhindley <s2257179@ed.ac.uk>
2024-05-24 12:04:15 +0100

    added table

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from shiny import App, render, ui
 
 app_ui = ui.page_fixed(
     ui.input_slider("val", "Slider label", min=0, max=100, value=50),
-    ui.output_text_verbatim("slider_val")
+    ui.output_text_verbatim("slider_val"),
+    ui.output_table("table_with_numbers")
 )
 
 def server(input, output, session):
     @render.text
     def slider_val():
         return f"Slider value: {input.val()}"
+    
+    @render.table
+    def table_with_numbers():
+        return [1,2,3]
 
 app = App(app_ui, server)
