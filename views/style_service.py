2bf1819fccb48724fd641323257586fe1fb9f425
Keith Douglas <keith@keithdouglas.co.uk>
2024-09-12 11:08:16 +0100

    course_as_card with css classes

--- views/style_service.py
+++ views/style_service.py
@@ -30,9 +30,6 @@ class StyleService:
         return [theme
          for theme in StyleService.get_themes()
           if theme.id == theme_id][0]
-    
-    def style_meta_box_half_bottom():
-        return "right: 0px; bottom: 0px; position:absolute;"
    
     def single_theme(theme_id, how_many_vertical, text=None):
         if text == None:
@@ -55,7 +52,7 @@ class StyleService:
                         # ui.div(f"{course_info.credits} cred"),
                           style="right: 0px; top: 0px; position: absolute;"),
                      ),
-                StyleService.box_of_themes(course_info.themes, StyleService.style_meta_box_half_bottom())
+                StyleService.box_of_themes(course_info.themes, "meta-box-half-bottom")
             ).add_class("meta-info-box"), 
             more_info_card 
         )
@@ -75,38 +72,36 @@ class StyleService:
             for theme_name in theme_counts_dict],
         )
 
-    def box_of_themes(themes, extra_styles = ""):
+    def box_of_themes(themes, css_class = ""):
         return ui.div([StyleService.single_theme(theme, len(themes)) 
                        for theme in themes], 
-                      style= StyleService.style_theme_box(len(themes)) +extra_styles)
+                      style= StyleService.style_theme_box(len(themes))
+                    ).add_class(css_class)
 
 
     def name_shorter(long_name):
         shorter_name =  long_name.replace("health and social care", "H&SC").replace("Health and Social Care", "H&SC")
         shorter_name = shorter_name.replace("Introduction", "Intro")
         return shorter_name
-    
-    def style_disabled_background():
-        return "background-color: #f4f4f4;"
 
     def course_as_card(course_info, show = True, buttons = [], dissertation = False, selected = False):
         name_label = StyleService.name_shorter(course_info.name) #+ " " + self.course_info.id
-        extra_styles = "padding-bottom: 80px" if dissertation else ""
-        extra_styles += StyleService.style_disabled_background() if selected else ""
+        
+        css_class = "course-box"
+        css_class += " bottom-padding-80" if dissertation else ""
+        css_class += " disabled-background" if selected else ""
 
         return ui.div( 
                         ui.div(  name_label, "*" if course_info.is_compulsory_course else None),
                         ui.div( 
                             ui.div(  *[button for button in buttons]   ),
-                            style = "margin:0px; padding:0px" ,
+                            style = "margin:0px; padding:0px",
                             hidden = (dissertation)
                         ),
                         StyleService.box_of_course_metainfo(course_info) ,
                         # StyleService.box_of_themes(self.course_info.themes),
-                  
-                         style= extra_styles,
                          hidden = (not show)
-        ).add_class("course-box")
+        ).add_class(css_class)
 
 
 
@@ -127,7 +122,7 @@ class StyleService:
                                 ui.div(ui.tags.b("Has prerequisites: "), course_info.has_pre_req_id ) if len(course_info.has_pre_req_id)>0 else None,
                                 ui.div(ui.tags.b("Themes: "), ", ".join(course_info.themes)),
                                 ui.div(ui.tags.b("Programming in: "), ", ".join(course_info.prog_lang)) if len(course_info.prog_lang)>0 else None,
-                                ui.div( StyleService.box_of_themes(course_info.themes, StyleService.style_meta_box_half_bottom())),
+                                ui.div( StyleService.box_of_themes(course_info.themes, "meta-box-half-bottom")),
                                 ui.row(ui.tags.a("View this course on DRPS", href=course_info.link, target="_blank"))
                             ))
         return more_info_card
