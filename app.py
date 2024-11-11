4a1d9adab99178f1bc8a4911a4e51ed7c3f6d7f8
drpawelo <drpawelo@gmail.com>
2024-11-11 12:37:33 +0000

    version bump

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from views.style_service import StyleService
 from htmltools import head_content
 
 
-version = "1.5.2.2" # major.sprint.prodrelease.devrelease 
+version = "1.5.3.3" # major.sprint.prodrelease.devrelease 
 # i.e. when releasing to dev, increase devrelease number, when releasing to prod, increase prodrelease number
     
 app_ui = ui.page_fixed(
